c810bfe25774ead8aee46ff5ef4c1673781b146b
FrozenPyrozen <kiril.kohan@gmail.com>
2019-10-19 18:26:21 -0400

    separate values in hook

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import {
+  requestPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from 'expo-location';
+
+export default callback => {
+  const [err, setErr] = useState(null);
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        callback
+      );
+    } catch (error) {
+      setErr(error);
+    }
+  };
+
+  return [err];
+};
