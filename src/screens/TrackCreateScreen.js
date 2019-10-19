114c576f70d3c134f303d203b512c62bdcb69525
FrozenPyrozen <kiril.kohan@gmail.com>
2019-10-19 15:08:30 -0400

    add mock location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
+import { requestPermissionsAsync } from 'expo-location';
 
 import Map from '../components/Map';
 
+// Development purposes only! Delete before production
+import '../_mockLocation';
+
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+    } catch (error) {
+      setErr(error);
+    }
+  };
+
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h2 style={styles.text}>
         Create a Track
       </Text>
       <Map />
+      {err ? (
+        <Text style={styles.error}>Please enable location services</Text>
+      ) : null}
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  error: {
+    color: 'red',
+    fontSize: 20,
+  },
+});
 
 export default TrackCreateScreen;
