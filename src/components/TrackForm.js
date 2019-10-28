9ed0e23d9ba5d805f95a9e3c3cedbb590cde06a8
FrozenPyrozen <kiril.kohan@gmail.com>
2019-10-27 22:01:59 -0400

    add recording sbtn

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import { View, StyleSheet } from 'react-native';
 import Spacer from './Spacer';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackForm = ({}) => {
+  const {
+    state: { name, recording, locations },
+    startRecording,
+    stopRecording,
+    changeName,
+  } = useContext(LocationContext);
+
   return (
     <>
       <Spacer>
-        <Input placeholder="Enter name" />
+        <Input
+          placeholder="Enter name"
+          value={name}
+          onChangeText={changeName}
+        />
       </Spacer>
       <Spacer>
-        <Button title="Start Recording" />
+        <Button
+          title={recording ? 'Stop' : 'Start Recording'}
+          onPress={recording ? stopRecording : startRecording}
+        />
       </Spacer>
     </>
   );
