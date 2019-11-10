519b0cf7da22f5616f5e7dcf01a876cb9e8c17bf
FrozenPyrozen <kiril.kohan@gmail.com>
2019-11-09 20:57:10 -0500

    add fetch tracs request

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -3,12 +3,21 @@ import trackerApi from '../api/tracker';
 
 const trackReducer = (state, action) => {
   switch (action.type) {
+    case 'fetch_tracks':
+      return action.payload;
     default:
       return state;
   }
 };
 
-const fetchTracks = dispatch => () => {};
+const fetchTracks = dispatch => async () => {
+  try {
+    const response = trackerApi.get('tracks');
+    dispatch({ type: 'fetch_tracks', payload: response.data });
+  } catch (error) {
+    console.error(error);
+  }
+};
 const createTrack = dispatch => async (name, locations) => {
   try {
     await trackerApi.post('/tracks', { name, locations });
